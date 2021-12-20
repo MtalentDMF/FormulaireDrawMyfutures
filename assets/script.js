ad9596df98af6a02d65de271db85d7bfb9ca3043
Salim <tizi_salim@yahoo.fr>
2021-12-20 15:31:29 +0100

    Modif US Controle des saisies du profil (nom,prenom,CP,genre,email) cf US Identité contact. Sans modif des class et id du français à l'anglais ni des getElementByName en querySelector

--- assets/script.js
+++ assets/script.js
@@ -1,159 +1,247 @@
-/* US NUMERO 1, puis 2 
+/* US NUMERO 1 et  2 
 VERIFICATION DE LA RECEPTION DES DONNES SOUMISES PAR LE FORMULAIRE
 */
 
 
-let myresult = document.getElementById('myresult');
-let myresult2 = document.getElementById('myresult2');
+let resultFormProfile = document.getElementById('resultFormProfile');
+let resultFormProfessional = document.getElementById('resultFormProfessional');
 let myButton = document.getElementById('buttonSubmit');
 let trancheAge = document.getElementsByName('boutonradio1');
-let genre = document.getElementsByName('genre');
+let gender = document.getElementsByName('gender');
 let etude = document.getElementsByName('etude');
 let situationPro = document.getElementsByName('situationPro');
-// let situationProText = document.getElementById('situationProText').value;
-//Jai enlevé la variable situationProText pourtant, l'element est quand meme recuperé 
+let nom = document.getElementById("nom");
+let prenom = document.getElementById("prenom");
+let email = document.getElementById("email");
+let CP = document.getElementById("CP");
+let validEmail = document.getElementById("validEmail");
+let champOb = 'Champs obligatoire';
 
-//POUR FAIRE DISPARAITRE UN ELEMENT DE MA PAGE HTML AU CHARGEMENT DE LA PAGE
+// let nameProfil = document.getElementById('nom').value;
+// let situationProText = document.getElementById('situationProText').value;
+
+//FONCTION DE VALIDATION DE L'EMAIL, JAI AUSSI INTEGRE LA VALIDATION QUE LE EMAIL NEST PAS VIDE
+
+const Emailvalidation = () => {
+    // let email 	= document.formulaire.EMAIL.value;
+
+    let verif = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/
+    if (email.value.length===0)
+        validEmail.textContent = champOb;
+    else if (verif.exec(email.value) == null) //email.value.length!==0
+    {
+        validEmail.textContent = "Email non valide";
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
+//ESSAYER DE CREER LES FONCTIONS POUR LES CHAMPS NOMS PRENOMS CODEPOSTAL 
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('demarrage du fichier');
-    document.getElementById('resultatAttendu').style.display = "none";
-    document.getElementById('resultatAttendu2').style.display = "none";
-    console.log('after display none');
-});
+    //JE FAIS DISPARAITRE LES DIV
+    document.getElementById('resultFormProfileStyle').classList.remove('block');
+    document.getElementById('resultFormProfileStyle').classList.add('none');
+    document.getElementById('resultFormProfessionalStyle').classList.remove('block');
+    document.getElementById('resultFormProfessionalStyle').classList.add('none');
 
-myButton.addEventListener('click', e => {
-    e.preventDefault();
-    //JE FAIS APPARAITRE LES DIV 
-    document.getElementById('resultatAttendu').style.display = "block";
-    document.getElementById('resultatAttendu2').style.display = "block";
-    myresult.textContent += 'nom: ' + document.getElementById('nom').value;
-    myresult.textContent += ', prenom: ' + document.getElementById('prenom').value;
-    myresult.textContent += ', email : ' + document.getElementById('email').value;
-    myresult.textContent += ', CP : ' + document.getElementById('CP').value;
-    for(i =0; i<trancheAge.length;i++)
-    {
-        if(trancheAge[i].checked)
-        myresult.textContent += ", tranche d'age : "  + trancheAge[i].value;
-    }
-    /* ---------------------------------------------------------------------------------- 
-    C'est un bouton radio, je créer une boucle pour récuperer la valeur de mon bouton grâce à la méthode checked. myresult.textConten += ', ' + document.getElementsByName('boutonradio1');
-    -------------------------------------------------------------------------------------
-    */
-    for(i =0; i<genre.length;i++)
-    {
-        if(genre[i].checked)
-        myresult.textContent += ', genre : ' + genre[i].value;
-    }
-    /* ----------------------------------------------------------------------------------
-    J'ai créer ma boucle sur cet exemple :
-    var ele = document.getElementsByName('gender');
-        for(i = 0; i < ele.length; i++) {
-             if(ele[i].checked)
-             document.getElementById("result").innerHTML
-                     = "Gender: "+ele[i].value;
-    -------------------------------------------------------------------------------------
-
-    JAURAI PU LE FAIRE AVEC UN TAG NAME SUR LES INPUT POUR RECUPERER TOUS LES INPUT 
-
-    function displayRadioValue() {
-            document.getElementById("result").innerHTML = "";
-            var ele = document.getElementsByTagName('input');
-              
+    myButton.addEventListener('click', e => {
+        e.preventDefault();
+        //JE FAIS APPARAITRE LES DIV 
+        document.getElementById('resultFormProfileStyle').classList.remove('none');
+        document.getElementById('resultFormProfileStyle').classList.add('block');
+        document.getElementById('resultFormProfessionalStyle').classList.remove('none');
+        document.getElementById('resultFormProfessionalStyle').classList.add('block');
+        
+        resultFormProfile.textContent += 'nom: ' + document.getElementById('nom').value;
+        resultFormProfile.textContent += ', prenom: ' + document.getElementById('prenom').value;
+        resultFormProfile.textContent += ', email : ' + document.getElementById('email').value;
+        resultFormProfile.textContent += ', CP : ' + document.getElementById('CP').value;
+        for(i =0; i<trancheAge.length;i++)
+        {
+            if(trancheAge[i].checked)
+            resultFormProfile.textContent += ", tranche d'age : "  + trancheAge[i].value;
+        }
+        /* ---------------------------------------------------------------------------------- 
+        C'est un bouton radio, je créer une boucle pour récuperer la valeur de mon bouton grâce à la méthode checked. resultFormProfile.textConten += ', ' + document.getElementsByName('boutonradio1');
+        -------------------------------------------------------------------------------------
+        */
+        for(i =0; i<gender.length;i++)
+        {
+            if(gender[i].checked)
+            resultFormProfile.textContent += ', genre : ' + gender[i].value;
+        }
+        /* ----------------------------------------------------------------------------------
+        J'ai créer ma boucle sur cet exemple :
+        var ele = document.getElementsByName('gender');
             for(i = 0; i < ele.length; i++) {
+                 if(ele[i].checked)
+                 document.getElementById("result").innerHTML
+                         = "Gender: "+ele[i].value;
+        -------------------------------------------------------------------------------------
+    
+        JAURAI PU LE FAIRE AVEC UN TAG NAME SUR LES INPUT POUR RECUPERER TOUS LES INPUT 
+    
+        function displayRadioValue() {
+                document.getElementById("result").innerHTML = "";
+                var ele = document.getElementsByTagName('input');
                   
-                if(ele[i].type="radio") {
-                  
-                    if(ele[i].checked)
-                        document.getElementById("result").innerHTML
-                                += ele[i].name + " Value: "
-                                + ele[i].value + "<br>";
+                for(i = 0; i < ele.length; i++) {
+                      
+                    if(ele[i].type="radio") {
+                      
+                        if(ele[i].checked)
+                            document.getElementById("result").innerHTML
+                                    += ele[i].name + " Value: "
+                                    + ele[i].value + "<br>";
+                    }
                 }
+            }
+    
+        */
+    
+        for (i = 0; i < etude.length; i++) {
+            if (etude[i].checked)
+                resultFormProfessional.textContent += ', niveau etude : ' + etude[i].value;
+        }
+    
+        // le domaine d'etude est obligatoirement renseigné
+    
+        resultFormProfessional.textContent += ', domaine etude : ' + document.getElementById('domEtude').value;
+        
+        for (i = 0; i < situationPro.length; i++) {
+            if (situationPro[i].checked)
+                resultFormProfessional.textContent += ', situation Pro : ' + situationPro[i].value;
+        }
+    
+        // Pour afficher le contenue du text area uniquement s'il y a une valeur.
+    
+        if(situationProText.value.length>0){
+            console.log('after if situationProText.value = "' + situationProText.value + '"');
+            // JAI RAJOUTE DES GUILLEMETS POUR VERIFIER QU'IL Y AVAIT BIEN DU CONTENU
+            resultFormProfessional.textContent += ', situation pro texte :  ' + situationProText.value;
+        
+        }
+        else
+            console.log('after else situationProText.value = ' + situationProText.value);
+        // SYSTEMATIQUEMENT RAJOUTER DU CONSOLE LOG LORSQU'IL Y A UN BUG. 
+        
+        resultFormProfessional.textContent += ', Poste avant reconversion : ' + document.getElementById('posteAvantReconv').value;
+    
+        /*
+        ---------------------------------------------------------------------------------
+        USER STORY 3 verification des champs du formulaire
+        ---------------------------------------------------------------------------------
+        */ 
+        
+        let validName = document.getElementById("validName");
+        let validPrenom = document.getElementById("validPrenom");
+        let validCP = document.getElementById("validCP");
+    
+        /*
+        -----------------------------------------------------------------------------------------------
+        Si le champs est vide et s'il ne respecte pas les caractères attendus.
+        ESSAYER DE CREER UNE FONCTION QUI FAIT LE TAFF CAR ON UTILISE 4 FOIS LA MEME CHOSE
+        -----------------------------------------------------------------------------------------------
+        */
+
+        let regexCarac = /^[a-zA-Z-\s]+$/;
+        let regexNumber = /^[0-9]+$/;
+        
+        
+        if (nom.value.length===0) {
+            validName.textContent = champOb;
+        }
+        else if(regexCarac.test(nom.value) == false){
+            console.log('after else if myRegex.text');
+            validName.textContent = "le nom doit comporter des lettres et des tirets uniquement";
+        }
+
+        if (prenom.value.length===0) {
+            validPrenom.textContent = champOb;
+        }
+        else if(regexCarac.test(prenom.value) == false){
+            console.log('after else if myRegex.text');
+            validPrenom.textContent = "le prénom doit comporter des lettres et des tirets uniquement";
+        }
+
+        if (CP.value.length===0) {
+            validCP.textContent = champOb;
+        }
+        else if(regexNumber.test(CP.value) == false){
+            console.log('after else if myRegex.text');
+            validCP.textContent = "le code postal doit comporter des chiffres uniquement";
+        }
+
+        //
+        //Si le bouton radio n'est pas coché
+        //
+        
+        
+        // let emptyCase = document.getElementById("emptyCase");
+        // let boolEmptyCase = 1;
+    
+        // for(i= 0; i<trancheAge.length; i++) {
+        //     if(boolEmptyCase == 1 && !trancheAge[i].checked){
+        //         console.log('after if trancheAge[i].checked');
+        //         emptyCase.textContent = 'veuillez cocher une case';
+        //         boolEmptyCase=0;
+        //     }
+        //     else{
+        //         console.log('je sors de la boucle');
+        //         return false;
+        //     }
+        // }
+        
+        //
+        // Version simplifié
+        //
+    
+        //Pour la tranche d'age
+    
+    
+        let emptyCase = document.getElementById("emptyCase");
+        
+        for(i= 0; i<trancheAge.length; i++) {
+            if(!trancheAge[i].checked){
+                console.log('after if trancheAge[i].checked');
+                emptyCase.textContent = 'Cette question est obligatoire';
+            }
+            else{
+                console.log('je sors de la boucle');
+                return false;
+            }
+        }
+    
+        //Pour le gender 
+    
+        let emptyCaseGender = document.getElementById("emptyCaseGender");
+        
+        for(i= 0; i<gender.length; i++) {
+            if(!gender[i].checked){
+                console.log('after if gender[i].checked');
+                emptyCaseGender.textContent = 'Cette question est obligatoire';
+            }
+            else{
+                console.log('je sors de la boucle');
+                return false;
             }
         }
 
-    */
+        //VALIDATION DE L'EMAIL PAR LA FONCTION
 
-    for (i = 0; i < etude.length; i++) {
-        if (etude[i].checked)
-            myresult2.textContent += ', niveau etude : ' + etude[i].value;
-    }
-
-    myresult2.textContent += ', domaine etude : ' + document.getElementById('domEtude').value;
-    
-    for (i = 0; i < situationPro.length; i++) {
-        if (situationPro[i].checked)
-            myresult2.textContent += ', situation Pro : ' + situationPro[i].value;
-    }
-
-    // Pour afficher le contenue du text area uniquement s'il y a une valeur.
-
-    if(situationProText.value){
-        console.log('after if situationProText.value = "' + situationProText.value + '"');
-        // JAI RAJOUTE DES GUILLEMETS POUR VERIFIER QU'IL Y AVAIT BIEN DU CONTENU
-        myresult2.textContent += ', situation pro texte :  ' + situationProText.value;
-    
-    }
-    else
-        console.log('after else situationProText.value = ' + situationProText.value);
-    // SYSTEMATIQUEMENT RAJOUTER DU CONSOLE LOG LORSQU'IL Y A UN BUG. 
-    
-    myresult2.textContent += ', Poste avant reconversion : ' + document.getElementById('posteAvantReconv').value;
-
-    // USER STORY 3 
-    
-    let champOb = 'Champs obligatoire';
-    let nom = document.getElementById("nom");
-    let prenom = document.getElementById("prenom");
-    let email = document.getElementById("email");
-    let CP = document.getElementById("CP");
-    let validNom = document.getElementById("validNom");
-    let validPrenom = document.getElementById("validPrenom");
-    let validEmail = document.getElementById("validEmail");
-    let validCP = document.getElementById("validCP");
-
-
-    //Si le champs est vide
-
-    if (nom.value==false) {
-        console.log('after if nom.value==false');
-        validNom.textContent = champOb;
-    }
-    if (prenom.value==false) {
-        console.log('after if prenom.value==false');
-        validPrenom.textContent = champOb;
-    }
-    if (email.value==false) { 
-        console.log('after if email.value==false');
-        validEmail.textContent = champOb;
-    }
-    if (CP.value==false) {
-        console.log('after if CP.value==false');
-        validCP.textContent = champOb;
-    }
-
-    //Si le bouton radio n'est pas coché
-
-    // let cocheCase = document.getElementById("cocheCase");
- 
-    // for (i = 0; i < trancheAge.length; i++) {
-    //     console.log('after for trancheAge cocheCase');
-    //     if (!trancheAge[i].checked) {
-    //         console.log('after if cocheCase')
-    //         cocheCase.textContent = 'Veuillez cocher une case';
-    //     }
-    // }
-
-    // Il rentre dans la boucle et affiche systematiquement cocher une case dés qu'il y a une case non coché. Il faudrait qu'il sorte de la boucle immédiatement apres avoir trouvé une case cochée.
-
-    
-    validNom.textContent = 'Veuillez-saisir des caractères uniquements';
-
-    validPrenom.textContent = 'Veuillez-saisir des caractères uniquements';
-
-    validCP.textContent = "Veuillez saisir un Code postal valide"
-
-    validEmail.textContent = "L'adresse email n'est pas valide";
+        Emailvalidation();
+        
+        
+        
+    });
 });
+
+
 
 
 
