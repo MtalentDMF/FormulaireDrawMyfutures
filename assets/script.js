66c36835cb52c9ab2bc3d267b106b4c9aeec9173
Salim <tizi_salim@yahoo.fr>
2021-12-22 12:14:25 +0100

    re: changement du design + regex mises à jour

--- assets/script.js
+++ assets/script.js
@@ -5,40 +5,80 @@ VERIFICATION DE LA RECEPTION DES DONNES SOUMISES PAR LE FORMULAIRE
 
 let resultFormProfile = document.getElementById('resultFormProfile');
 let resultFormProfessional = document.getElementById('resultFormProfessional');
-let myButton = document.getElementById('buttonSubmit');
-let trancheAge = document.getElementsByName('boutonradio1');
+let buttonSubmit = document.getElementById('buttonSubmit');
+let ageRange = document.getElementsByName('ageRange');
 let gender = document.getElementsByName('gender');
 let etude = document.getElementsByName('etude');
-let situationPro = document.getElementsByName('situationPro');
-let nom = document.getElementById("nom");
-let prenom = document.getElementById("prenom");
+let ProSituation = document.getElementsByName('ProSituation');
+// let situationProText = document.getElementById('situationProText').value;
+let name = document.getElementById("name");
+let firstName = document.getElementById("firstName");
 let email = document.getElementById("email");
 let CP = document.getElementById("CP");
-let validEmail = document.getElementById("validEmail");
-let champOb = 'Champs obligatoire';
 
-// let nameProfil = document.getElementById('nom').value;
-// let situationProText = document.getElementById('situationProText').value;
+//VERIFICATION DES CHAMPS OBLIGATOIRE ET DES EXPRESSIONS OBLIGATOIRES (REGEX)
+let champOb = 'Champs obligatoire';
+let validEmail = document.getElementById("validEmail");
+let validName = document.getElementById("validName");
+let validFirstName = document.getElementById("validFirstName");
+let validCP = document.getElementById("validCP");
+
 
 //FONCTION DE VALIDATION DE L'EMAIL, JAI AUSSI INTEGRE LA VALIDATION QUE LE EMAIL NEST PAS VIDE
 
-const Emailvalidation = () => {
+const emailValidation = () => {
     // let email 	= document.formulaire.EMAIL.value;
+    
+    //Rajout du "." et du "_"
 
-    let verif = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/
-    if (email.value.length===0)
+    let verif = /^[\u00C0-\u00FFa-zA-Z0-9-_._]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    if (email.value.length===0){
         validEmail.textContent = champOb;
+    }
     else if (verif.exec(email.value) == null) //email.value.length!==0
     {
         validEmail.textContent = "Email non valide";
         return false;
     }
     else {
-        return true;
+         return true;
     }
 
 }
+// 
+// FONCTION DE VALIDATION DE NOM, DE PRENOM, DE CODE POSTAL
+const nameValidation = () =>{
+    let regexCarac = /^[a-zA-Z-_\u00C0-\u00FF\s]+$/;
+    if (name.value.length===0) {
+        validName.textContent = champOb;
+    }
+    else if(regexCarac.test(name.value) == false){
+        console.log('after else if myRegex.text');
+        validName.textContent = "le nom doit comporter des lettres et des tirets uniquement";
+    }
+}
 
+const firstNameValidation = () =>{
+    let regexCarac = /^[a-zA-Z-_\u00C0-\u00FF\s]+$/;
+    if (firstName.value.length===0) {
+        validFirstName.textContent = champOb;
+    }
+    else if(regexCarac.test(firstName.value) == false){
+        console.log('after else if myRegex.text');
+        validFirstName.textContent = "le prénom doit comporter des lettres et des tirets uniquement";
+    }
+}
+
+const codePostalValidation = () =>{
+        let regexNumber = /^[0-9]+$/;
+        if (CP.value.length===0) {
+            validCP.textContent = champOb;
+        }
+        else if(regexNumber.test(CP.value) == false){
+            console.log('after else if myRegex.text');
+            validCP.textContent = "le code postal doit comporter des chiffres uniquement";
+        }
+    }
 //ESSAYER DE CREER LES FONCTIONS POUR LES CHAMPS NOMS PRENOMS CODEPOSTAL 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -49,25 +89,27 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('resultFormProfessionalStyle').classList.remove('block');
     document.getElementById('resultFormProfessionalStyle').classList.add('none');
 
-    myButton.addEventListener('click', e => {
+    buttonSubmit.addEventListener('click', e => {
         e.preventDefault();
         //JE FAIS APPARAITRE LES DIV 
+
         document.getElementById('resultFormProfileStyle').classList.remove('none');
         document.getElementById('resultFormProfileStyle').classList.add('block');
         document.getElementById('resultFormProfessionalStyle').classList.remove('none');
         document.getElementById('resultFormProfessionalStyle').classList.add('block');
         
-        resultFormProfile.textContent += 'nom: ' + document.getElementById('nom').value;
-        resultFormProfile.textContent += ', prenom: ' + document.getElementById('prenom').value;
+
+        resultFormProfile.textContent += 'nom: ' + document.getElementById('name').value;
+        resultFormProfile.textContent += ', prenom: ' + document.getElementById('firstName').value;
         resultFormProfile.textContent += ', email : ' + document.getElementById('email').value;
         resultFormProfile.textContent += ', CP : ' + document.getElementById('CP').value;
-        for(i =0; i<trancheAge.length;i++)
+        for(i =0; i<ageRange.length;i++)
         {
-            if(trancheAge[i].checked)
-            resultFormProfile.textContent += ", tranche d'age : "  + trancheAge[i].value;
+            if(ageRange[i].checked)
+            resultFormProfile.textContent += ", tranche d'age : "  + ageRange[i].value;
         }
         /* ---------------------------------------------------------------------------------- 
-        C'est un bouton radio, je créer une boucle pour récuperer la valeur de mon bouton grâce à la méthode checked. resultFormProfile.textConten += ', ' + document.getElementsByName('boutonradio1');
+        C'est un bouton radio, je créer une boucle pour récuperer la valeur de mon bouton grâce à la méthode checked. resultFormProfile.textConten += ', ' + document.getElementsByName('ageRange');
         -------------------------------------------------------------------------------------
         */
         for(i =0; i<gender.length;i++)
@@ -103,7 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
     
         */
-    
+            
         for (i = 0; i < etude.length; i++) {
             if (etude[i].checked)
                 resultFormProfessional.textContent += ', niveau etude : ' + etude[i].value;
@@ -113,11 +155,11 @@ document.addEventListener('DOMContentLoaded', () => {
     
         resultFormProfessional.textContent += ', domaine etude : ' + document.getElementById('domEtude').value;
         
-        for (i = 0; i < situationPro.length; i++) {
-            if (situationPro[i].checked)
-                resultFormProfessional.textContent += ', situation Pro : ' + situationPro[i].value;
+        for (i = 0; i < ProSituation.length; i++) {
+            if (ProSituation[i].checked)
+                resultFormProfessional.textContent += ', situation Pro : ' + ProSituation[i].value;
         }
-    
+        
         // Pour afficher le contenue du text area uniquement s'il y a une valeur.
     
         if(situationProText.value.length>0){
@@ -138,72 +180,44 @@ document.addEventListener('DOMContentLoaded', () => {
         ---------------------------------------------------------------------------------
         */ 
         
-        let validName = document.getElementById("validName");
-        let validPrenom = document.getElementById("validPrenom");
-        let validCP = document.getElementById("validCP");
-    
-        /*
-        -----------------------------------------------------------------------------------------------
-        Si le champs est vide et s'il ne respecte pas les caractères attendus.
-        ESSAYER DE CREER UNE FONCTION QUI FAIT LE TAFF CAR ON UTILISE 4 FOIS LA MEME CHOSE
-        -----------------------------------------------------------------------------------------------
-        */
-
-        let regexCarac = /^[a-zA-Z-\s]+$/;
-        let regexNumber = /^[0-9]+$/;
+        // Si le champs est vide et s'il ne respecte pas les caractères attendus.
         
+        nameValidation();
+
+        firstNameValidation();
+
+        emailValidation();
         
-        if (nom.value.length===0) {
-            validName.textContent = champOb;
-        }
-        else if(regexCarac.test(nom.value) == false){
-            console.log('after else if myRegex.text');
-            validName.textContent = "le nom doit comporter des lettres et des tirets uniquement";
-        }
-
-        if (prenom.value.length===0) {
-            validPrenom.textContent = champOb;
-        }
-        else if(regexCarac.test(prenom.value) == false){
-            console.log('after else if myRegex.text');
-            validPrenom.textContent = "le prénom doit comporter des lettres et des tirets uniquement";
-        }
-
-        if (CP.value.length===0) {
-            validCP.textContent = champOb;
-        }
-        else if(regexNumber.test(CP.value) == false){
-            console.log('after else if myRegex.text');
-            validCP.textContent = "le code postal doit comporter des chiffres uniquement";
-        }
+        codePostalValidation();
 
         //
         //Si le bouton radio n'est pas coché
         //
-    
+        
         //Pour la tranche d'age
-    
+
+        // let emptyCase = document.getElementById("emptyCase");
+        // let boolEmptyCase = 1;
+        // for (i = 0; i < ageRange.length; i++) {
+        //     if (boolEmptyCase == 1 && !ageRange[i].checked) {
+        //         console.log('after if !ageRange[i].checked');
+        //         emptyCase.textContent = 'Cette question est obligatoire';
+        //         boolEmptyCase = 0;
+        //     }
+        //     else {
+        //         console.log('je sors de la boucle');
+        //         return false;
+        //     }
+        // }
+
         let emptyCase = document.getElementById("emptyCase");
-        
-        for(i= 0; i<trancheAge.length; i++) {
-            if(!trancheAge[i].checked){
-                console.log('after if trancheAge[i].checked');
+        let boolEmptyCase = 1;
+
+        for(i= 0; i<ageRange.length; i++) {
+            if(!ageRange[i].checked && boolEmptyCase == 1){
+                console.log('after if !ageRange[i].checked');
                 emptyCase.textContent = 'Cette question est obligatoire';
-            }
-            else{
-                console.log('je sors de la boucle');
-                return false;
-            }
-        }
-    
-        //Pour le gender 
-    
-        let emptyCaseGender = document.getElementById("emptyCaseGender");
-        
-        for(i= 0; i<gender.length; i++) {
-            if(!gender[i].checked){
-                console.log('after if gender[i].checked');
-                emptyCaseGender.textContent = 'Cette question est obligatoire';
+                boolEmptyCase = 0;
             }
             else{
                 console.log('je sors de la boucle');
@@ -211,11 +225,18 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        //VALIDATION DE L'EMAIL PAR LA FONCTION
-
-        Emailvalidation();
+        // let emptyCaseGender = document.getElementById("emptyCaseGender");
         
-        
+        // for(i= 0; i<gender.length; i++) {
+        //     if(!gender[i].checked){
+        //         console.log('after if gender[i].checked');
+        //         emptyCaseGender.textContent = 'Cette question est obligatoire';
+        //     }
+        //     else{
+        //         console.log('je sors de la boucle');
+        //         return false;
+        //     }
+        // }
         
     });
 });
