b4de9aad8b185a4d307b835163b2aaf3f7a76471
Salim <tizi_salim@yahoo.fr>
2022-02-09 16:48:19 +0100

    bug données mises à jour, affichage en ligne de la situation professionnelle, début de message d'erreur qui disparait, manque la supression du message d'erreur sur les checkbox

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 /* US NUMERO 1 et  2 
-VERIFICATION DE LA RECEPTION DES DONNES SOUMISES PAR LE FORMULAIRE
+AFFICHAGE DES DONNES SOUMISES PAR LE FORMULAIRE AU BAS DE LA PAGE
 */
 
 
@@ -7,12 +7,11 @@ let resultFormProfile = document.getElementById('resultFormProfile');
 let resultFormProfessional = document.getElementById('resultFormProfessional');
 let buttonSubmit = document.getElementById('buttonSubmit');
 let ageRange = document.getElementsByName('ageRange');
-// ageRange avec un querySelector 
 let gender = document.getElementsByName('gender');
-//gender avec un querySelector
 let study = document.getElementsByName('study');
 let ProSituation = document.getElementsByName('ProSituation');
-// let situationProText = document.getElementById('situationProText').value;
+// let situationProText = document.getElementById('situationProText');
+// let jobBeforeRetraining = document.getElementById('jobBeforeRetraining');
 let name = document.getElementById("name");
 let firstName = document.getElementById("firstName");
 let email = document.getElementById("email");
@@ -25,56 +24,96 @@ let validName = document.getElementById("validName");
 let validFirstName = document.getElementById("validFirstName");
 let validCP = document.getElementById("validCP");
 
-
-//FONCTION DE VALIDATION DE L'EMAIL ET DE VERIFICATION QUE L'EMAIL NEST PAS VIDE
-
-const emailValidation = () => {
-    // let email 	= document.formulaire.EMAIL.value;
-    
-    //Rajout du "." et du "_"
-
-    let verif = /^[\u00C0-\u00FFa-zA-Z0-9-_._]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    if (verif.exec(email.value) == null) //email.value.length!==0
-    {
-        validEmail.textContent = "Email non valide";
-        return false;
-    }
-    else {
-         return true;
-    }
-
-}
-// 
-// FONCTION DE VALIDATION DE NOM, DE PRENOM, DE CODE POSTAL
+// FONCTION DE VALIDATION DE NOM, DE PRENOM, DE CODE POSTAL ET D'EMAIL
 
 let regexCarac = /^[a-zA-Z-_\u00C0-\u00FF\s]+$/;//la regex est la meme dans nom et prenom
 
 const nameValidation = () =>{
-    if(regexCarac.test(name.value) == false){
-        console.log('after else if myRegex.text');
+    if(regexCarac.test(name.value)){
+        validName.textContent ="";
+    }
+    else{ 
         validName.textContent = "le nom doit comporter des lettres et des tirets uniquement";
     }
 }
 
 const firstNameValidation = () =>{
-    if(regexCarac.test(firstName.value) == false){
-        console.log('after else if myRegex.text');
+    if(regexCarac.test(firstName.value)){
+        validFirstName.textContent = "";
+    }
+    else{
         validFirstName.textContent = "le prénom doit comporter des lettres et des tirets uniquement";
+    }
+}
+
+const emailValidation = () => {
+    let verif = /^[\u00C0-\u00FFa-zA-Z0-9-_._]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+    if(verif.exec(email.value)){
+        validEmail.textContent = "";
+    }
+    else{
+        validEmail.textContent = "Email non valide";
     }
 }
 
 const codePostalValidation = () =>{
         let regexNumber = /^[0-9]+$/;
-        if(regexNumber.test(CP.value) == false){
-            console.log('after else if myRegex.text');
+        if(regexNumber.test(CP.value)){
+            validCP.textContent = "";
+        }
+        else{
             validCP.textContent = "le code postal doit comporter des chiffres uniquement";
         }
-    }
-//ESSAYER DE CREER LES FONCTIONS POUR LES CHAMPS NOMS PRENOMS CODEPOSTAL 
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('demarrage du fichier');
-    //JE FAIS DISPARAITRE LES DIV
+    
+    //La verification de caracteres des champs se fait dans le input
+    /*
+    ---------------------------------------------------------
+    Je crée un event au changement de l'input cest à dire des quon entre une donnée dans le input
+    ---------------------------------------------------------
+    */
+
+    let form = document.querySelector('form');
+
+    form.name.addEventListener('change', function() {
+        nameValidation();
+    });
+
+    form.firstName.addEventListener('change', function() {
+        firstNameValidation();
+    });
+
+    form.email.addEventListener('change', function() {
+        emailValidation();
+    });
+
+    form.CP.addEventListener('change', function() {
+        codePostalValidation();
+    });
+
+    /**
+     * 
+    ---------------------------------------------------------
+    Boucle à inserer dans un event pour vérifier qu'une case du checkbox est bien coché et enlever le message d'erreur potentiel champs obligatoire
+    ---------------------------------------------------------
+     * 
+    for (i = 0; i < ageRange.length; i++) {
+        if (ageRange[i].checked)
+            emptyCase.textContent = "";
+    }
+
+    for (i = 0; i < gender.length; i++) {
+        if (gender[i].checked)
+            emptyCaseGender.textContent = "";
+    }
+     * 
+     */
+
+    //JE FAIS DISPARAITRE LES DIV DE RECUPERATION DES DONNEES AU BAS DE MON FORMULAIRE
+
     document.getElementById('resultFormProfileStyle').classList.remove('block');
     document.getElementById('resultFormProfileStyle').classList.add('none');
     document.getElementById('resultFormProfessionalStyle').classList.remove('block');
@@ -93,20 +132,24 @@ document.addEventListener('DOMContentLoaded', () => {
         resultFormProfile.innerHTML += "Prenom: " + document.getElementById('firstName').value + '<br>';
         resultFormProfile.innerHTML += 'Email : ' + document.getElementById('email').value + '<br>';
         resultFormProfile.innerHTML += 'CP : ' + document.getElementById('CP').value + '<br>';
-        for(i =0; i<ageRange.length;i++)
-        {
-            if(ageRange[i].checked)
-                resultFormProfile.innerHTML += "Tranche d'age : "  + ageRange[i].value + '<br>';
-        }
+
         /* ---------------------------------------------------------------------------------- 
-        C'est un bouton radio, je créer une boucle pour récuperer la valeur de mon bouton grâce à la méthode checked.
+        C'est un bouton radio, je créer une boucle pour récuperer la/les valeur(s) de mon bouton grâce à la méthode checked.
         -------------------------------------------------------------------------------------
         */
-        for(i =0; i<gender.length;i++)
+       
+        for(i =0; i<ageRange.length;i++)
+       {
+           if(ageRange[i].checked)
+               resultFormProfile.innerHTML += "Tranche d'age : "  + ageRange[i].value + '<br>';
+       }
+
+       for(i =0; i<gender.length;i++)
         {
             if(gender[i].checked)
                 resultFormProfile.innerHTML += 'Genre : ' + gender[i].value + '<br>';
         }
+
         /* 
         JAURAI PU LE FAIRE AVEC UN TAG NAME SUR LES INPUT POUR RECUPERER TOUS LES INPUT 
     
@@ -127,34 +170,41 @@ document.addEventListener('DOMContentLoaded', () => {
             }
     
         */
+        
+        // J'ajoute un champs 'niveau etude' vide pour que ça ne rajoute pas des champs à chaque click du bouton envoyer sur le formulaire
+        
+        resultFormProfessional.innerHTML = 'Niveau etude : <br>';
             
         for (i = 0; i < study.length; i++) {
             if (study[i].checked)
                 resultFormProfessional.innerHTML = 'Niveau etude : ' + study[i].value + '<br>';
-        }
+        } 
     
-        // le domaine d'etude est obligatoirement renseigné
-    
-        resultFormProfessional.innerHTML = 'Domaine etude : ' + document.getElementById('studyArea').value + '<br>';
+        resultFormProfessional.innerHTML += 'Domaine etude : ' + document.getElementById('studyArea').value + '<br>';
         
+        //J'ajoute un champ situation professionnelle en dehors du for pour qu'il n'apparaisse qu'une fois et que les situation pro apparaissent à la suite
+
+        resultFormProfessional.innerHTML += 'Situation professionnelle : ';
+
         for (i = 0; i < ProSituation.length; i++) {
             if (ProSituation[i].checked)
-                resultFormProfessional.innerHTML += 'Situation Pro : ' + ProSituation[i].value + '<br>';
+                resultFormProfessional.innerHTML += ProSituation[i].value + ' , ';
         }
+
+        resultFormProfessional.innerHTML += '<br>' + 'Situation pro autre :  ' + situationProText.value + '<br>';
         
         // Pour afficher le contenue du text area uniquement s'il y a une valeur.
-    
-        if(situationProText.value.length>0){
-            console.log('after if situationProText.value = "' + situationProText.value + '"');
-            // JAI RAJOUTE DES GUILLEMETS POUR VERIFIER QU'IL Y AVAIT BIEN DU CONTENU
-            resultFormProfessional.innerHTML += 'Situation pro texte :  ' + situationProText.value + '<br>';
+        // if(situationProText.value.length>0){
+        //     console.log('after if situationProText.value = "' + situationProText.value + '"');
+        //     // JAI RAJOUTE DES GUILLEMETS POUR VERIFIER QU'IL Y AVAIT BIEN DU CONTENU
+        //     resultFormProfessional.innerHTML += '<br>' + 'Situation pro texte :  ' + situationProText.value + '<br>';
         
-        }
-        else
-            console.log('after else situationProText.value = ' + situationProText.value);
+        // }
+        // else
+        //     console.log('after else situationProText.value = ' + situationProText.value);
         // SYSTEMATIQUEMENT RAJOUTER DU CONSOLE LOG LORSQU'IL Y A UN BUG. 
         
-        resultFormProfessional.innerHTML += 'Poste avant reconversion : ' + document.getElementById('jobBeforeRetraining').value + '<br>';
+        resultFormProfessional.innerHTML += 'Poste avant reconversion : ' + jobBeforeRetraining.value + '<br>';
     
         /*
         ---------------------------------------------------------------------------------
@@ -167,10 +217,6 @@ document.addEventListener('DOMContentLoaded', () => {
         if (name.value.length===0) {
             validName.textContent = RequiredField;
         }
-        else if(name.value.length){
-            true;
-        }
-
 
         if (firstName.value.length===0) {
             validFirstName.textContent = RequiredField;
@@ -203,7 +249,7 @@ document.addEventListener('DOMContentLoaded', () => {
         //     }
         // }
 
-        //Par Algorithme
+        //Version simplifiée
 
         // Age
 
@@ -212,8 +258,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         for(i= 0; i<ageRange.length; i++) {
             if(ageRange[i].checked){
-                console.log('after if !ageRange[i].checked');
                 boolEmptyCase = true;
+                emptyCase.textContent = "";
             }
         }
         if(boolEmptyCase==false)
@@ -228,8 +274,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         for(i= 0; i<gender.length; i++) {
             if(gender[i].checked){
-                console.log('after if !gender[i].checked');
                 boolEmptyCaseG = true;
+                emptyCaseGender.textContent = "";
             }
         }
         if(boolEmptyCaseG==false)
@@ -239,31 +285,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             
     });
-
-        //La verification des champs doit se faire dans le input
-        /*
-        ---------------------------------------------------------
-        Je crée un nouvel envent au changement de l'input cest à dire des quon entre une donnée dans le input
-        ---------------------------------------------------------
-        */
-    
-        let form = document.querySelector('form');
-
-        form.name.addEventListener('change', function() {
-            nameValidation();
-        });
-
-        form.firstName.addEventListener('change', function() {
-            firstNameValidation();
-        });
-
-        form.email.addEventListener('change', function() {
-            emailValidation();
-        });
-
-        form.CP.addEventListener('change', function() {
-            codePostalValidation();
-        });
+        
 });
 
 
