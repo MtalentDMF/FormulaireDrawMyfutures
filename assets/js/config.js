9463cda421f03f4d77efd16a1a9001a3cc8f09c1
amedie <amedie@shapr.net>
2022-04-26 16:22:43 +0200

    revert

---
+++ assets/js/config.js
@@ -0,0 +1,20 @@
+let config = {
+    MY_AIRTABLE_API_KEY : 'keyJZoCXEOlsyQu2N',
+    MY_AIRTABLE_BDD_ID: 'apprgKZR6URooGqFe'
+}
+
+/** 
+ 
+
+Nouvelle BDD dont je suis la propriétaire : https://airtable.com/appTee4PZdkPIsowN/tblFi3dypqmAFoZ47/viwiEZoJZtf91k60m?blocks=hide
+API de la nouvelle BDD https://airtable.com/appTee4PZdkPIsowN/api/docs
+  
+  
+ nouveux accès à mettre en changeant tt les id des valeurs dans le html 
+
+    MY_AIRTABLE_API_KEY : 'keyWGodOGCXDa3E5l',
+    MY_AIRTABLE_BDD_ID: 'appTee4PZdkPIsowN'
+
+
+ * 
+ */
